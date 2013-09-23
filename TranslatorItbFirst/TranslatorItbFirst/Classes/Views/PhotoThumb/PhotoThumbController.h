b47ffeb94c3c8743163f29bbf1bd8cb650a0b494
zhron4x <zhr0n4x@gmail.com>
2013-09-23 15:37:09 +0300

    PhotoThumbView addition

---
+++ TranslatorItbFirst/TranslatorItbFirst/Classes/Views/PhotoThumb/PhotoThumbController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoThumbController.h
+//  TranslatorItbFirst
+//
+//  Created by Konstantin Paschenko on 23.09.13.
+//  Copyright (c) 2013 vexadev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoThumbController : UIView
+
+@end
---
+++ TranslatorItbFirst/TranslatorItbFirst/Classes/Views/PhotoThumb/PhotoThumbController.m
@@ -0,0 +1,31 @@
+//
+//  PhotoThumbController.m
+//  TranslatorItbFirst
+//
+//  Created by Konstantin Paschenko on 23.09.13.
+//  Copyright (c) 2013 vexadev. All rights reserved.
+//
+
+#import "PhotoThumbController.h"
+
+@implementation PhotoThumbController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
